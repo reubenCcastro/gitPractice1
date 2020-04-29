483700edbefcb6e36fb70d9a5fee8ea0390999ff
reubenCcastro <64111067+reubenCcastro@users.noreply.github.com>
2020-04-29 08:38:29 -0600

    Update Login.java

--- src/test/Login.java
+++ src/test/Login.java
@@ -4,5 +4,7 @@ public class Login {
 
     public static void main(String[] args) {
         System.out.println("Test");
+        
+        Can I update Github with IntelliJ also at the same time.
     }
 }
