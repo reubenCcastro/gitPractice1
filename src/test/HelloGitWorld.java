0aaf58adb06698f7c300e94e645e0f58fed2df61
reubenCcastro <64111067+reubenCcastro@users.noreply.github.com>
2020-04-29 08:26:03 -0600

    Update HelloGitWorld.java

--- src/test/HelloGitWorld.java
+++ src/test/HelloGitWorld.java
@@ -5,5 +5,7 @@ public class HelloGitWorld {
     public static void main(String[] args) {
 
         System.out.println("HelloGitWorld");
+        
+        //Justing testing to see if this message appears on IntelliJ since i mad chages on Github
     }
 }
