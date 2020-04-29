40956100b956586002d02375a0309ec248e39dfb
Reuben Castro <castro.reuben@gmail.com>
2020-04-29 08:40:45 -0600

    update

--- src/test/Login.java
+++ src/test/Login.java
@@ -5,6 +5,6 @@ public class Login {
     public static void main(String[] args) {
         System.out.println("Test");
         
-        Can I update Github with IntelliJ also at the same time.
+        //Can I update Github with IntelliJ also at the same time.
     }
 }
